c9779aefc59d6313cf5ddf44b0fbfa7e58ffa6dd
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-09-01 15:52:21 +0100

    almost there

--- PCI_wrapper.py
+++ PCI_wrapper.py
@@ -1,6 +1,13 @@
 import os
 import zarr
 
+from kerchunk.hdf import SingleHdf5ToZarr
+
+
+# example valid netCDF4 file
+# can be loaded through h5py
+cmip6_test_file = r"/home/valeriu/climate_data/CMIP6/CMIP/MPI-M/MPI-ESM1-2-HR/historical/r1i1p1f1/Omon/sos/gn/v20190710/sos_Omon_MPI-ESM1-2-HR_historical_r1i1p1f1_gn_198001-198412.nc"
+
 def build_zarr_dataset():
     """Create a zarr array and save it."""
     dsize = (60, 404, 802)
@@ -12,13 +19,51 @@ def build_zarr_dataset():
     zarr.save_array("example.zarr", z, compressor=None)
 
 
-def PCI_wrapper_standalone(chunk_selection):
-    """Build a wrapper of PartialChunkIterator and run it as standalone."""
-    from zarr import core
+def load_zarr_native():
+    """Load the Zarr file we have built, actual/native format."""
     zarr_dir = "./example.zarr"
     if not os.path.isdir(zarr_dir):
         build_zarr_dataset()
     ds = zarr.open("./example.zarr")
+
+    return ds
+
+
+def extract_dict(strarg):
+    """Extract a dict from a string-formatted dict."""
+    # FIXME this is a bit silly
+    items = strarg.split('"shape"')
+    return_dict = dict()
+    val = items[1].split(":")[1].strip()
+    val = val.split('"zarr_format"')[0].split(',')[0:3]
+    val = (int(val[0].lstrip('[').strip()),
+           int(val[1].strip()),
+           int(val[2].split("]")[0].strip()))
+    return_dict["shape"] = val
+
+    return return_dict
+
+
+def load_netcdf_zarr():
+    """Pass a netCDF4 file to be shaped as Zarr file."""
+    ds = SingleHdf5ToZarr(cmip6_test_file).translate()
+    chunks = extract_dict(ds['refs']['sos/.zarray'])["shape"]
+
+    store, chunk_store = dict(), dict()
+    ref_ds = zarr.create(chunks, chunks=chunks, compressor=None,
+                         dtype='f8', order='C',
+                         store=store, chunk_store=chunk_store)
+
+    return ref_ds
+
+
+def PCI_wrapper_standalone(chunk_selection, native_zarr=False):
+    """Build a wrapper of PartialChunkIterator and run it as standalone."""
+    from zarr import core
+    if not native_zarr:
+        ds = load_netcdf_zarr()
+    else:
+        ds = load_zarr_native()
     # functional arguments example
     # chunk_selection: (slice(0, 2, 1), slice(0, 4, 1), slice(0, 2, 1))
     pc = core.Array._process_chunk_V(ds, chunk_selection)
@@ -26,15 +71,16 @@ def PCI_wrapper_standalone(chunk_selection):
     return pc
 
 
-def PCI_wrapper(selection):
+def PCI_wrapper(selection, compute_data, native_zarr=False):
     """Use all the Zarr ins."""
     from zarr import core
-    zarr_dir = "./example.zarr"
-    if not os.path.isdir(zarr_dir):
-        build_zarr_dataset()
-    ds = zarr.open("./example.zarr")
+    if not native_zarr:
+        ds = load_netcdf_zarr()
+    else:
+        ds = load_zarr_native()
     pc = core.Array.get_orthogonal_selection(ds, selection,
-                                             out=None, fields=None)
+                                             out=None, fields=None,
+                                             compute_data=compute_data)
     return pc
 
 
@@ -42,16 +88,27 @@ def main():
     """Extract the needed info straight from Zarr."""
     # pass a chunk_selection and run the standalone function
     chunk_selection = (slice(0, 2, 1), slice(0, 4, 1), slice(0, 2, 1))
-    chunks, chunk_sel, PCI = PCI_wrapper_standalone(chunk_selection)
+    chunks, chunk_sel, PCI = PCI_wrapper_standalone(chunk_selection,
+                                                    native_zarr=False)
     print("Running the PCI wrapper as standalone:")
     print(f"Chunks {chunks}, chunk selection {chunk_sel}, PCI {list(PCI)}\n")
 
+    # toggle compute_data
+    # this turns on and off the actual data loading into store
+    compute_data = False
+
     # integrate it in the Zarr mechanism
-    data_selection, chunk_info = PCI_wrapper(chunk_selection)
+    data_selection, chunk_info = PCI_wrapper(chunk_selection,
+                                             compute_data,
+                                             native_zarr=False)
     print("Running the PCI wrapper integrated in Zarr.Array:")
     print(f"Data selection {data_selection}")
-    chunks, chunk_sel, PCI = chunk_info[0]
-    print(f"Chunks {chunks}, chunk selection {chunk_sel}, PCI {list(PCI)}\n")
+    chunks_full, chunk_sel_full, PCI_full = chunk_info[0]
+    print(f"Chunks {chunks}, chunk selection {chunk_sel_full}, PCI {list(PCI_full)}\n")
+
+    # cross-check
+    assert chunk_sel == chunk_sel_full
+    assert list(PCI) == list(PCI_full)
         
 
 
