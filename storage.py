d1ef35aef946d8932f016b506d99d5746837747d
Bryan Lawrence <b.n.lawrence@reading.ac.uk>
2022-09-29 10:49:26 +0100

    slight documentation improvements

--- storage.py
+++ storage.py
@@ -1,7 +1,20 @@
 from numcodecs.compat import ensure_ndarray
 
 def decode_chunk(rfile, offset, size, dtype, shape, order, chunk_selection, method=None):
-        """ We do our own read of chunks and decoding etc """
+        """ We do our own read of chunks and decoding etc 
+        
+        rfile - the actual file with the data 
+        offset, size - where and what we want ...
+        dtype - likely float32 in most cases. 
+        shape - will be a tuple, something like (3,3,1) 
+        order - typically 'C' for c-type ordering
+        chunk_selection - python slice tuples for each dimension, e.g.
+                           (slice(0, 2, 1), slice(1, 3, 1), slice(0, 1, 1))
+        method - computation desired 
+               (in this Python version it's an actual method, in 
+               storage implementations we'll change to controlled vocabulary)
+                        
+        """
        
         #fIXME: for the moment, open the file every time ... we might want to do that, or not
         with open(rfile,'rb') as open_file:
