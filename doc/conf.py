c36b0dd97f2dcf2fc725ce1502db97cddbd9df1d
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-10-13 16:12:12 +0100

    add logo

--- doc/conf.py
+++ doc/conf.py
@@ -165,7 +165,7 @@ html_short_title = "PyActiveStorage {0}".format(release)
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 # FIXME add a logo
-# html_logo = 
+html_logo = "figures/alpaca-logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
