e1a2c6ff9149e6112293f5e695d3c9587e5a6e51
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-10-13 15:15:06 +0100

    add to basic attrs test

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,5 +1,7 @@
 import activestorage
 
+from activestorage import Active as act
+
 
 # test version
 def test_version():
@@ -7,9 +9,23 @@ def test_version():
     assert activestorage.__version__ == "0.0.0"
     print(activestorage.__version__)
 
-# check class
+# check activestorage class
 def test_class_attrs():
     assert hasattr(activestorage, "Active")
     assert hasattr(activestorage, "active")
     assert hasattr(activestorage, "storage")
     assert hasattr(activestorage, "netcdf_to_zarr")
+
+# check Active class
+def test_active_class_attrs():
+    assert hasattr(act, "__new__")
+    assert hasattr(act, "__getitem__")
+    assert hasattr(act, "__init__")
+    assert hasattr(act, "_from_storage")
+    assert hasattr(act, "_get_active")
+    assert hasattr(act, "_get_selection")
+    assert hasattr(act, "_process_chunk")
+    assert hasattr(act, "_via_kerchunk")
+    assert hasattr(act, "components")
+    assert hasattr(act, "method")
+    assert hasattr(act, "ncvar")
