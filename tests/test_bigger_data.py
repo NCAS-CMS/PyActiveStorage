25a6ff492c16f9a08ff472143aaa85388086f24d
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-10-14 14:26:08 +0100

    big hefty netcdf file

---
+++ tests/test_bigger_data.py
@@ -0,0 +1,105 @@
+"""Create test data for heftier data tests."""
+import os
+import pytest
+
+import numpy as np
+from netCDF4 import Dataset
+
+from activestorage.active import Active
+
+
+def create_hyb_pres_file_without_ap(dataset, short_name):
+    """Create dataset without vertical auxiliary coordinate ``ap``."""
+    dataset.createDimension('time', size=1)
+    dataset.createDimension('lev', size=2)
+    dataset.createDimension('lat', size=3)
+    dataset.createDimension('lon', size=4)
+    dataset.createDimension('bnds', size=2)
+
+    # Dimensional variables
+    dataset.createVariable('time', np.float64, dimensions=('time',))
+    dataset.createVariable('lev', np.float64, dimensions=('lev',))
+    dataset.createVariable('lev_bnds', np.float64, dimensions=('lev', 'bnds'))
+    dataset.createVariable('lat', np.float64, dimensions=('lat',))
+    dataset.createVariable('lon', np.float64, dimensions=('lon',))
+    dataset.variables['time'][:] = [0.0]
+    dataset.variables['time'].standard_name = 'time'
+    dataset.variables['time'].units = 'days since 6543-2-1'
+    dataset.variables['lev'][:] = [1.0, 2.0]
+    dataset.variables['lev'].bounds = 'lev_bnds'
+    dataset.variables['lev'].standard_name = (
+        'atmosphere_hybrid_sigma_pressure_coordinate')
+    dataset.variables['lev'].units = '1'
+    dataset.variables['lev_bnds'][:] = [[0.5, 1.5], [1.5, 3.0]]
+    dataset.variables['lev_bnds'].standard_name = (
+        'atmosphere_hybrid_sigma_pressure_coordinate')
+    dataset.variables['lev_bnds'].units = '1'
+    dataset.variables['lat'][:] = [-30.0, 0.0, 30.0]
+    dataset.variables['lat'].standard_name = 'latitude'
+    dataset.variables['lat'].units = 'degrees_north'
+    dataset.variables['lon'][:] = [30.0, 60.0, 90.0, 120.0]
+    dataset.variables['lon'].standard_name = 'longitude'
+    dataset.variables['lon'].units = 'degrees_east'
+
+    # Coordinates for derivation of pressure coordinate
+    dataset.createVariable('b', np.float64, dimensions=('lev',))
+    dataset.createVariable('b_bnds', np.float64, dimensions=('lev', 'bnds'))
+    dataset.createVariable('ps', np.float64,
+                           dimensions=('time', 'lat', 'lon'))
+    dataset.variables['b'][:] = [0.0, 1.0]
+    dataset.variables['b_bnds'][:] = [[-1.0, 0.5], [0.5, 2.0]]
+    dataset.variables['ps'][:] = np.arange(1 * 3 * 4).reshape(1, 3, 4)
+    dataset.variables['ps'].standard_name = 'surface_air_pressure'
+    dataset.variables['ps'].units = 'Pa'
+    dataset.variables['ps'].additional_attribute = 'xyz'
+
+    # Variable
+    dataset.createVariable(short_name, np.float32,
+                           dimensions=('time', 'lev', 'lat', 'lon'))
+    dataset.variables[short_name][:] = np.full((1, 2, 3, 4), 0.0,
+                                               dtype=np.float32)
+    dataset.variables[short_name].standard_name = (
+        'cloud_area_fraction_in_atmosphere_layer')
+    dataset.variables[short_name].units = '%'
+
+
+def create_hyb_pres_file_with_a(dataset, short_name):
+    """Create netcdf file with issues in hybrid pressure coordinate."""
+    create_hyb_pres_file_without_ap(dataset, short_name)
+    dataset.createVariable('a', np.float64, dimensions=('lev',))
+    dataset.createVariable('a_bnds', np.float64, dimensions=('lev', 'bnds'))
+    dataset.createVariable('p0', np.float64, dimensions=())
+    dataset.variables['a'][:] = [1.0, 2.0]
+    dataset.variables['a_bnds'][:] = [[0.0, 1.5], [1.5, 3.0]]
+    dataset.variables['p0'][:] = 1.0
+    dataset.variables['p0'].units = 'Pa'
+    dataset.variables['lev'].formula_terms = 'p0: p0 a: a b: b ps: ps'
+    dataset.variables['lev_bnds'].formula_terms = (
+        'p0: p0 a: a_bnds b: b_bnds ps: ps')
+
+
+def save_cl_file_with_a(tmp_path):
+    """Create netcdf file for ``cl`` with ``a`` coordinate."""
+    save_path = tmp_path / 'common_cl_a.nc'
+    nc_path = os.path.join(save_path)
+    dataset = Dataset(nc_path, mode='w')
+    create_hyb_pres_file_with_a(dataset, 'cl')
+    dataset.close()
+    print(f"Saved {save_path}")
+    return str(save_path)
+
+
+def test_hefty_data(tmp_path):
+    ncfile = save_cl_file_with_a(tmp_path)
+    active = Active(ncfile, "cl")
+    active._version = 0
+    d = active[0:2, 4:6, 7:9]
+    mean_result = np.mean(d)
+
+    active = Active(ncfile, "cl")
+    active._version = 2
+    active.method = "mean"
+    active.components = True
+    result2 = active[0:2, 4:6, 7:9]
+    print(result2)
+    self.assertEqual(mean_result, result2["sum"]/result2["n"]) 
