0bce7ad31a4d4f4c7c68bd372a09265664294453
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-11-07 17:02:12 +0000

    add explicit unit test for storage

---
+++ tests/unit/test_storage.py
@@ -0,0 +1,17 @@
+import os
+import numpy as np
+import pytest
+
+import activestorage.storage as st
+
+
+def test_reduce_chunk():
+    """Test reduce chunk entirely."""
+    rfile = "tests/test_data/cesm2_native.nc"
+    offset = 2
+    size = 128
+    rc = st.reduce_chunk(rfile, offset, size,
+                         compression=None, filters=None,
+                         missing=True, dtype="i2", shape=(8, 8),
+                         order="C", chunk_selection=slice(0, 2, 1),
+                         method=None)
