6b20eb6b6252f2eebc6aa12b54439435cff5bbee
Valeriu Predoi <valeriu.predoi@gmail.com>
2022-11-08 14:26:48 +0000

    add tests for vmin and vmax

--- tests/unit/test_storage.py
+++ tests/unit/test_storage.py
@@ -59,3 +59,75 @@ def test_reduced_chunk_masked_data():
                          method=np.ma.mean)
     assert rc[0] == 249.45955882352942
     assert rc[1] == 680
+
+
+def test_reduced_chunk_fully_masked_data_fill():
+    """Test method with masked data."""
+    rfile = "tests/test_data/daily_data_fullmask.nc"
+    offset = 6911
+    size = 2976
+
+    # no compression
+    ch_sel = (slice(0, 62, 1), slice(0, 2, 1),
+              slice(0, 3, 1), slice(0, 2, 1))
+    rc = st.reduce_chunk(rfile, offset, size,
+                         compression=None, filters=None,
+                         missing=(None, 999.0, None, None),
+                         dtype="float32", shape=(62, 2, 3, 2),
+                         order="C", chunk_selection=ch_sel,
+                         method=np.ma.mean)
+    assert rc == 999.
+
+
+def test_reduced_chunk_fully_masked_data_missing():
+    """Test method with masked data."""
+    rfile = "tests/test_data/daily_data_fullmask.nc"
+    offset = 6911
+    size = 2976
+
+    # no compression
+    ch_sel = (slice(0, 62, 1), slice(0, 2, 1),
+              slice(0, 3, 1), slice(0, 2, 1))
+    rc = st.reduce_chunk(rfile, offset, size,
+                         compression=None, filters=None,
+                         missing=(999., None, None, None),
+                         dtype="float32", shape=(62, 2, 3, 2),
+                         order="C", chunk_selection=ch_sel,
+                         method=np.ma.mean)
+    assert rc == 999.
+
+
+def test_reduced_chunk_fully_masked_data_vmin():
+    """Test method with masked data."""
+    rfile = "tests/test_data/daily_data_fullmask.nc"
+    offset = 6911
+    size = 2976
+
+    # no compression
+    ch_sel = (slice(0, 62, 1), slice(0, 2, 1),
+              slice(0, 3, 1), slice(0, 2, 1))
+    rc = st.reduce_chunk(rfile, offset, size,
+                         compression=None, filters=None,
+                         missing=(None, None, 1000., None),
+                         dtype="float32", shape=(62, 2, 3, 2),
+                         order="C", chunk_selection=ch_sel,
+                         method=np.ma.mean)
+    assert rc == 900.
+
+
+def test_reduced_chunk_fully_masked_data_vmax():
+    """Test method with masked data."""
+    rfile = "tests/test_data/daily_data_fullmask.nc"
+    offset = 6911
+    size = 2976
+
+    # no compression
+    ch_sel = (slice(0, 62, 1), slice(0, 2, 1),
+              slice(0, 3, 1), slice(0, 2, 1))
+    rc = st.reduce_chunk(rfile, offset, size,
+                         compression=None, filters=None,
+                         missing=(None, None, None, 1.),
+                         dtype="float32", shape=(62, 2, 3, 2),
+                         order="C", chunk_selection=ch_sel,
+                         method=np.ma.mean)
+    assert rc == 1.1
