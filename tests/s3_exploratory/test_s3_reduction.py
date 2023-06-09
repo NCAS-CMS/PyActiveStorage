37ca643f08f646a0c835ecabed38ee18c66e0d19
Valeriu Predoi <valeriu.predoi@gmail.com>
2023-06-09 13:31:00 +0100

    run Active component too

--- tests/s3_exploratory/test_s3_reduction.py
+++ tests/s3_exploratory/test_s3_reduction.py
@@ -2,11 +2,33 @@ import os
 import numpy as np
 import pytest
 import s3fs
+import tempfile
 
+from activestorage.active import Active
+from activestorage.dummy_data import make_vanilla_ncdata
 import activestorage.storage as st
 from activestorage.s3 import reduce_chunk as s3_reduce_chunk
+from numpy.testing import assert_array_equal
 
 from config_minio import *
+
+
+def make_tempfile():
+    """Make dummy data."""
+    temp_folder = tempfile.mkdtemp()
+    s3_testfile = os.path.join(temp_folder,
+                               's3_test_bizarre.nc')  # Bryan likes this name
+    print(f"S3 Test file is {s3_testfile}")
+    if not os.path.exists(s3_testfile):
+        make_vanilla_ncdata(filename=s3_testfile)
+
+    local_testfile = os.path.join(temp_folder,
+                                  'local_test_bizarre.nc')  # Bryan again
+    print(f"Local Test file is {local_testfile}")
+    if not os.path.exists(local_testfile):
+        make_vanilla_ncdata(filename=local_testfile)
+
+    return s3_testfile, local_testfile
 
 
 def upload_to_s3(server, username, password, bucket, object, rfile):
@@ -21,6 +43,36 @@ def upload_to_s3(server, username, password, bucket, object, rfile):
     s3_fs.put_file(rfile, os.path.join(bucket, object))
 
 
+def test_Active():
+    """
+    Shows what we expect an active example test to achieve and provides "the right answer" 
+    """
+    # make dummy data
+    s3_testfile, local_testfile = make_tempfile()
+
+    # put s3 dummy data onto S3. then rm from local
+    object = os.path.basename(s3_testfile)
+    upload_to_s3(S3_URL, S3_ACCESS_KEY, S3_SECRET_KEY,
+                 S3_BUCKET, object, s3_testfile)
+    os.remove(s3_testfile)
+
+    # run Active on s3 file
+    # Active should be run with USE_S3 set to true here
+    active = Active(s3_testfile, "data")
+    active._version = 0
+    d = active[0:2, 4:6, 7:9]
+    mean_result = np.mean(d)
+
+    # run Active on local file
+    active = Active(local_testfile, "data")
+    active._version = 2
+    active.method = "mean"
+    active.components = True
+    result2 = active[0:2, 4:6, 7:9]
+    print(result2)
+    assert_array_equal(mean_result, result2["sum"]/result2["n"])
+
+
 def test_s3_reduce_chunk():
     """Unit test for s3_reduce_chunk."""
     rfile = "tests/test_data/cesm2_native.nc"
@@ -32,6 +84,11 @@ def test_s3_reduce_chunk():
     upload_to_s3(S3_URL, S3_ACCESS_KEY, S3_SECRET_KEY,
                  S3_BUCKET, object, rfile)
     
+    # remove file during test session to be sure
+    # workflow uses uploaded file to S3 bucket
+    os.remove(rfile)
+
+    # call s3_reduce_chunk
     tmp, count = s3_reduce_chunk(S3_ACTIVE_STORAGE_URL, S3_ACCESS_KEY,
                                  S3_SECRET_KEY, S3_URL, S3_BUCKET,
                                  object, offset, size,
