7cc9fa3d96585d7f560b9205b5beb1a2276f65e1
Mark Goddard <mark@stackhpc.com>
2023-06-19 16:34:01 +0100

    Use local storage for missing data reference
    It seems that h5py does not handle the missing data correctly.

--- tests/test_missing.py
+++ tests/test_missing.py
@@ -18,7 +18,7 @@ def _doit(testfile, **kwargs):
     Compare and contrast vanilla mean with actual means
     """
     uri = utils.write_to_storage(testfile)
-    active = Active(uri, "data", utils.get_storage_type())
+    active = Active(testfile, "data")
     active._version = 0
     d = active[0:2, 4:6, 7:9]
     mean_result = np.mean(d)
@@ -30,31 +30,37 @@ def _doit(testfile, **kwargs):
     result2 = active[0:2, 4:6, 7:9]
     np.testing.assert_array_equal(mean_result, result2["sum"]/result2["n"])
 
+@pytest.mark.xfail(USE_S3, reason="Missing data not supported in S3 yet")
 def test_partially_missing_data(tmp_path):
     testfile = str(tmp_path / 'test_partially_missing_data.nc')
     r = dd.make_partially_missing_ncdata(testfile)
     _doit(testfile, missing_value=-999.)
 
+@pytest.mark.xfail(USE_S3, reason="Missing data not supported in S3 yet")
 def test_missing(tmp_path):
     testfile = str(tmp_path / 'test_missing.nc')
     r = dd.make_partially_missing_ncdata(testfile)
     _doit(testfile, missing_value=-999.)
 
+# FIXME: why does it pass for S3?
 def test_fillvalue(tmp_path):
     testfile = str(tmp_path / 'test_fillvalue.nc')
     r = dd.make_fillvalue_ncdata(testfile)
     _doit(testfile, _FillValue=-999.)
 
+# FIXME: why does it pass for S3?
 def test_validmin(tmp_path):
     testfile = str(tmp_path / 'test_validmin.nc')
     r = dd.make_validmin_ncdata(testfile)
     _doit(testfile, valid_min=-1.)
 
+# FIXME: why does it pass for S3?
 def test_validmax(tmp_path):
     testfile = str(tmp_path / 'test_validmax.nc')
     r = dd.make_validmax_ncdata(testfile)
     _doit(testfile, valid_max=1.2 * 10 ** 3)
 
+# FIXME: why does it pass for S3?
 def test_validrange(tmp_path):
     testfile = str(tmp_path / 'test_validrange.nc')
     r = dd.make_validrange_ncdata(testfile)
