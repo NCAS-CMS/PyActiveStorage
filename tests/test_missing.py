e6fd2b3be5c971752c95210f527aa77178e50606
Valeriu Predoi <valeriu.predoi@gmail.com>
2023-07-07 17:48:04 +0100

    write to storage per test and kill current tests see results

--- tests/test_missing.py
+++ tests/test_missing.py
@@ -17,54 +17,60 @@ def _doit(testfile):
     """ 
     Compare and contrast vanilla mean with actual means
     """
-    uri = utils.write_to_storage(testfile)
-    active = Active(uri, "data", utils.get_storage_type())
+    active = Active(testfile, "data", utils.get_storage_type())
     active._version = 0
     d = active[0:2, 4:6, 7:9]
     mean_result = np.ma.mean(d)
     print("Bogstandard numpy mean", mean_result)
 
-    active = Active(uri, "data", utils.get_storage_type())
+    active = Active(testfile, "data", utils.get_storage_type())
     active._version = 2
     active.method = "mean"
     active.components = True
     result2 = active[0:2, 4:6, 7:9]
     print("Active result mean", result2["sum"]/result2["n"])
     np.testing.assert_array_equal(mean_result, result2["sum"]/result2["n"])
+    print(x)
 
 
 # @pytest.mark.skipif(USE_S3, reason="Missing data not supported in S3 yet")
 def test_partially_missing_data(tmp_path):
     testfile = str(tmp_path / 'test_partially_missing_data.nc')
     r = dd.make_partially_missing_ncdata(testfile)
+    utils.write_to_storage(testfile)
     _doit(testfile)
 
 # @pytest.mark.skipif(USE_S3, reason="Missing data not supported in S3 yet")
 def test_missing(tmp_path):
     testfile = str(tmp_path / 'test_missing.nc')
     r = dd.make_missing_ncdata(testfile)
+    utils.write_to_storage(testfile)
     _doit(testfile)
 
 # @pytest.mark.skipif(USE_S3, reason="Missing data not supported in S3 yet")
 def test_fillvalue(tmp_path):
     testfile = str(tmp_path / 'test_fillvalue.nc')
     r = dd.make_fillvalue_ncdata(testfile)
+    utils.write_to_storage(testfile)
     _doit(testfile)
 
 # @pytest.mark.skipif(USE_S3, reason="Missing data not supported in S3 yet")
 def test_validmin(tmp_path):
     testfile = str(tmp_path / 'test_validmin.nc')
     r = dd.make_validmin_ncdata(testfile)
+    utils.write_to_storage(testfile)
     _doit(testfile)
 
 # @pytest.mark.skipif(USE_S3, reason="Missing data not supported in S3 yet")
 def test_validmax(tmp_path):
     testfile = str(tmp_path / 'test_validmax.nc')
     r = dd.make_validmax_ncdata(testfile)
+    utils.write_to_storage(testfile)
     _doit(testfile)
 
 # @pytest.mark.skipif(USE_S3, reason="Missing data not supported in S3 yet")
 def test_validrange(tmp_path):
     testfile = str(tmp_path / 'test_validrange.nc')
     r = dd.make_validrange_ncdata(testfile)
+    utils.write_to_storage(testfile)
     _doit(testfile)
