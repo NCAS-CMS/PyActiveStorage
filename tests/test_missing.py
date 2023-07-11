ccc9cdf1d1bf373f957a83ce66f786bac73744c5
Valeriu Predoi <valeriu.predoi@gmail.com>
2023-07-11 13:12:35 +0100

    compactify one

--- tests/test_missing.py
+++ tests/test_missing.py
@@ -15,33 +15,37 @@ from activestorage import dummy_data as dd
 import utils
 
 
-"""
-There is a high level of duplication in this test module:
-this is to avert any issues of I/O toestepping from h5netcdf
-when running with USE_S3=True; this will probably have to change
-when we start using netCDF4python with S3-enabled
-"""
+def load_dataset(testfile):
+    """Load data as netCDF4.Dataset."""
+    ds = Dataset(testfile)
+    actual_data = ds["data"][:]
+    ds.close()
+
+    return actual_data
+
+
+def active_zero(testfile):
+    """Run Active with no active storage (version=0)."""
+    active = Active(testfile, "data", utils.get_storage_type())
+    active._version = 0
+    d = active[0:2, 4:6, 7:9]
+
+    return d
 
 def test_partially_missing_data(tmp_path):
     testfile = str(tmp_path / 'test_partially_missing_data.nc')
     r = dd.make_partially_missing_ncdata(testfile)
 
     # retrieve the actual numpy-ed result
-    ds = Dataset(testfile)
-    actual_data = ds["data"][:]
-    ds.close()
+    actual_data = load_dataset(testfile)
     unmasked_numpy_mean = np.mean(actual_data[0:2, 4:6, 7:9])
     print("Numpy masked result (mean)", unmasked_numpy_mean)
 
     # write file to storage
     testfile = utils.write_to_storage(testfile)
 
-    # run the two Active instances: transfer data and do active storage
-    active = Active(testfile, "data", utils.get_storage_type())
-    active._version = 0
-    d = active[0:2, 4:6, 7:9]
     # NOT numpy masked to check for correct Active behaviour
-    no_active_mean = np.mean(d)
+    no_active_mean = np.mean(active_zero(testfile))
     print("No active storage result (mean)", no_active_mean)
 
     active = Active(testfile, "data", utils.get_storage_type())
@@ -66,22 +70,15 @@ def test_missing(tmp_path):
     r = dd.make_missing_ncdata(testfile)
 
     # retrieve the actual numpy-ed result
-    ds = Dataset(testfile)
-    actual_data = ds["data"][:]
-    ds.close()
+    actual_data = load_dataset(testfile)
     unmasked_numpy_mean = np.mean(actual_data[0:2, 4:6, 7:9])
     print("Numpy masked result (mean)", unmasked_numpy_mean)
 
     # write file to storage
     testfile = utils.write_to_storage(testfile)
 
-    # run the two Active instances: transfer data and do active storage
-    active = Active(testfile, "data", utils.get_storage_type())
-    active._version = 0
-    d = active[0:2, 4:6, 7:9]
-
     # NOT numpy masked to check for correct Active behaviour
-    no_active_mean = np.mean(d)
+    no_active_mean = np.mean(active_zero(testfile))
     print("No active storage result (mean)", no_active_mean)
 
     active = Active(testfile, "data", utils.get_storage_type())
@@ -111,9 +108,7 @@ def test_fillvalue(tmp_path):
     r = dd.make_fillvalue_ncdata(testfile)
 
     # retrieve the actual numpy-ed result
-    ds = Dataset(testfile)
-    actual_data = ds["data"][:]
-    ds.close()
+    actual_data = load_dataset(testfile)
     actual_data = np.ma.masked_where(actual_data == -999., actual_data)
     unmasked_numpy_mean = np.mean(actual_data[0:2, 4:6, 7:9])
     print("Numpy masked result (mean)", unmasked_numpy_mean)
@@ -121,10 +116,7 @@ def test_fillvalue(tmp_path):
     # write file to storage
     testfile = utils.write_to_storage(testfile)
 
-    # run the two Active instances: transfer data and do active storage
-    active = Active(testfile, "data", utils.get_storage_type())
-    active._version = 0
-    d = active[0:2, 4:6, 7:9]
+    d = active_zero(testfile)
     # d is unmasked, contains fill_values=-999 just like any other data points
     d = np.ma.masked_where(d == -999., d)
 
@@ -167,9 +159,7 @@ def test_validmin(tmp_path):
     r = dd.make_validmin_ncdata(testfile)
 
     # retrieve the actual numpy-ed result
-    ds = Dataset(testfile)
-    actual_data = ds["data"][:]
-    ds.close()
+    actual_data = load_dataset(testfile)
     actual_data = np.ma.masked_where(actual_data < 751., actual_data)
     unmasked_numpy_mean = np.mean(actual_data[0:2, 4:6, 7:9])
     print("Numpy masked result (mean)", unmasked_numpy_mean)
@@ -177,10 +167,7 @@ def test_validmin(tmp_path):
     # write file to storage
     testfile = utils.write_to_storage(testfile)
 
-    # run the two Active instances: transfer data and do active storage
-    active = Active(testfile, "data", utils.get_storage_type(), valid_min=751.)
-    active._version = 0
-    d = active[0:2, 4:6, 7:9]
+    d = active_zero(testfile)
     # d is masked but with valid vals also <751.
     d = np.ma.masked_where(d < 751., d)
 
@@ -223,9 +210,7 @@ def test_validmax(tmp_path):
     r = dd.make_validmax_ncdata(testfile)
 
     # retrieve the actual numpy-ed result
-    ds = Dataset(testfile)
-    actual_data = ds["data"][:]
-    ds.close()
+    actual_data = load_dataset(testfile)
     actual_data = np.ma.masked_where(actual_data > 850., actual_data)
     unmasked_numpy_mean = np.mean(actual_data[0:2, 4:6, 7:9])
     print("Numpy masked result (mean)", unmasked_numpy_mean)
@@ -233,10 +218,7 @@ def test_validmax(tmp_path):
     # write file to storage
     testfile = utils.write_to_storage(testfile)
 
-    # run the two Active instances: transfer data and do active storage
-    active = Active(testfile, "data", utils.get_storage_type(), valid_max=850.)
-    active._version = 0
-    d = active[0:2, 4:6, 7:9]
+    d = active_zero(testfile)
     # d is masked but with valid vals also >800.
     d = np.ma.masked_where(d > 850., d)
 
@@ -279,9 +261,7 @@ def test_validrange(tmp_path):
     r = dd.make_validrange_ncdata(testfile)
 
     # retrieve the actual numpy-ed result
-    ds = Dataset(testfile)
-    actual_data = ds["data"][:]
-    ds.close()
+    actual_data = load_dataset(testfile)
     actual_data = np.ma.masked_where(750. > actual_data, actual_data)
     actual_data = np.ma.masked_where(850. < actual_data, actual_data)
     unmasked_numpy_mean = np.mean(actual_data[0:2, 4:6, 7:9])
@@ -290,10 +270,7 @@ def test_validrange(tmp_path):
     # write file to storage
     testfile = utils.write_to_storage(testfile)
 
-    # run the two Active instances: transfer data and do active storage
-    active = Active(testfile, "data", utils.get_storage_type(), valid_min=750., valid_max=850.)
-    active._version = 0
-    d = active[0:2, 4:6, 7:9]
+    d = active_zero(testfile)
     d = np.ma.masked_where(750. > d, d)
     d = np.ma.masked_where(850. < d, d)
 
