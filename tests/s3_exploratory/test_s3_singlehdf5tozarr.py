fd04fc1fd9c9b662fd27c3afc2273deb87ed923e
Valeriu Predoi <valeriu.predoi@gmail.com>
2023-09-22 15:03:35 +0100

    simplify test and make use of local test file

--- tests/s3_exploratory/test_s3_singlehdf5tozarr.py
+++ tests/s3_exploratory/test_s3_singlehdf5tozarr.py
@@ -1,11 +1,18 @@
 import s3fs
 
+from pathlib import Path
 from kerchunk.hdf import SingleHdf5ToZarr
 
 
-def test_s3_SingleHdf5ToZarr(s3_file):
+@pytest.fixture
+def test_data_path():
+    """Path to test data."""
+    return Path(__file__).resolve().parent / 'test_data'
+
+
+def test_s3_SingleHdf5ToZarr():
     """Check Kerchunk's SingleHdf5ToZarr when S3."""
-    s3_file = 
+    s3_file = "s3://pyactivestorage/s3_test_bizarre.nc"
     fs = s3fs.S3FileSystem(key=S3_ACCESS_KEY,
                            secret=S3_SECRET_KEY,
                            client_kwargs={'endpoint_url': S3_URL},
@@ -17,9 +24,9 @@ def test_s3_SingleHdf5ToZarr(s3_file):
                                     inline_threshold=0)
 
 
-def test_local_SingleHdf5ToZarr():
+def test_local_SingleHdf5ToZarr(test_data_path):
     """Check Kerchunk's SingleHdf5ToZarr when NO S3."""
-    local_file = 
+    local_file = str(test_data_path / "test_bizarre.nc")
     fs = fsspec.filesystem('')
     with fs.open(local_file, 'rb') as localfile:
         h5chunks = SingleHdf5ToZarr(localfile, local_file,
