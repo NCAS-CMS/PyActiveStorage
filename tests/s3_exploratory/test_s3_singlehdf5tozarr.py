b2325f9dfe2d104a7aca2a30cdb9fbc076782df2
Valeriu Predoi <valeriu.predoi@gmail.com>
2023-09-22 16:49:02 +0100

    plop all tests there

--- tests/s3_exploratory/test_s3_singlehdf5tozarr.py
+++ tests/s3_exploratory/test_s3_singlehdf5tozarr.py
@@ -38,19 +38,7 @@ def test_local_SingleHdf5ToZarr(test_data_path):
                                     inline_threshold=0)
 
 
-def test_Active():
-    """
-    Test truly Active with an S3 file.
-    """
-    # run Active on s3 file
-    s3_file = "s3://pyactivestorage/s3_test_bizarre_large.nc"
-    active = Active(s3_file, "data", "s3")
-    active.method = "mean"
-    active.components = True
-    result1 = active[0:2, 4:6, 7:9]
-
-
-def test_Active_v0():
+def test_Active_s3_v0():
     """
     Test truly Active with an S3 file.
     """
@@ -61,8 +49,32 @@ def test_Active_v0():
     active.components = True
     result1 = active[0:2, 4:6, 7:9]
 
+def test_Active_s3_v2():
+    """
+    Test truly Active with an S3 file.
+    """
+    # run Active on s3 file
+    s3_file = "s3://pyactivestorage/s3_test_bizarre_large.nc"
+    active = Active(s3_file, "data", "s3")
+    active._version = 2
+    active.method = "mean"
+    active.components = True
+    result1 = active[0:2, 4:6, 7:9]
 
-def test_Active_v1(test_data_path):
+
+def test_Active_local_v0(test_data_path):
+    """
+    Test pulling the data locally.
+    """
+    # run Active on local file
+    local_file = str(test_data_path / "test_bizarre.nc")
+    active = Active(local_file, "data")
+    active._version = 0
+    active.components = True
+    result2 = active[0:2, 4:6, 7:9]
+
+
+def test_Active_local_v1(test_data_path):
     """
     Test pulling the data locally.
     """
@@ -70,6 +82,19 @@ def test_Active_v1(test_data_path):
     local_file = str(test_data_path / "test_bizarre.nc")
     active = Active(local_file, "data")
     active._version = 1
+    active.method = "mean"
+    active.components = True
+    result2 = active[0:2, 4:6, 7:9]
+
+
+def test_Active_local_v2(test_data_path):
+    """
+    Test pulling the data locally.
+    """
+    # run Active on local file
+    local_file = str(test_data_path / "test_bizarre.nc")
+    active = Active(local_file, "data")
+    active._version = 2
     active.method = "mean"
     active.components = True
     result2 = active[0:2, 4:6, 7:9]
