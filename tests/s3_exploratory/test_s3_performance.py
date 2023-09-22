f536247cbce06dc094d49dece62b90cb198d7a31
Valeriu Predoi <valeriu.predoi@gmail.com>
2023-09-22 14:22:45 +0100

    barebones singlehsf5zarr tested

--- tests/s3_exploratory/test_s3_performance.py
+++ tests/s3_exploratory/test_s3_performance.py
@@ -11,6 +11,7 @@ from activestorage.dummy_data import make_vanilla_ncdata
 import activestorage.storage as st
 from activestorage.reductionist import reduce_chunk as reductionist_reduce_chunk
 from activestorage.netcdf_to_zarr import gen_json
+from kerchunk.hdf import SingleHdf5ToZarr
 
 from numpy.testing import assert_allclose, assert_array_equal
 from pathlib import Path
@@ -77,66 +78,48 @@ def local_file():
     return create_files()[1]
 
 
-def test_Active(s3_file):
-    """
-    Test truly Active with an S3 file.
-    """
-    print("S3 file uri", s3_file)
+#def test_Active(s3_file):
+#    """
+#    Test truly Active with an S3 file.
+#    """
+#    print("S3 file uri", s3_file)
+#
+#    # run Active on s3 file
+#    active = Active(s3_file, "data", "s3")
+#    active.method = "mean"
+#    result1 = active[0:2, 4:6, 7:9]
+#    print(result1)
 
-    # run Active on s3 file
-    active = Active(s3_file, "data", "s3")
-    active.method = "mean"
-    result1 = active[0:2, 4:6, 7:9]
-    print(result1)
 
-
-def test_no_Active(local_file):
-    """
-    Test pulling the data locally.
-    """
-    # run Active on local file
-    active = Active(local_file, "data")
-    active._version = 1
-    active.method = "mean"
-    active.components = True
-    result2 = active[0:2, 4:6, 7:9]
-    print(result2)
+#def test_no_Active(local_file):
+#    """
+#    Test pulling the data locally.
+#    """
+#    # run Active on local file
+#    active = Active(local_file, "data")
+#    active._version = 1
+#    active.method = "mean"
+#    active.components = True
+#    result2 = active[0:2, 4:6, 7:9]
+#    print(result2)
 
 
 def test_s3_SingleHdf5ToZarr(s3_file):
     """Check Kerchunk's SingleHdf5ToZarr when S3."""
-    fs = s3fs.S3FileSystem(key=S3_ACCESS_KEY,  # eg "minioadmin" for Minio
-                           secret=S3_SECRET_KEY,  # eg "minioadmin" for Minio
-                           client_kwargs={'endpoint_url': S3_URL},  # eg "http://localhost:9000" for Minio
-                           # caching stuff - unclear
-                           default_fill_cache=True,
-                           default_cache_type="readahead"
-                           # this combo produces factor 20x slower runs
-                           # default_fill_cache=False,  # for no caching
-                           # default_cache_type="none"
+    fs = s3fs.S3FileSystem(key=S3_ACCESS_KEY,
+                           secret=S3_SECRET_KEY,
+                           client_kwargs={'endpoint_url': S3_URL},
+                           default_fill_cache=False,
+                           default_cache_type="none"
     )
-    so = {
-        "mode": 'rb',
-        "default_fill_cache": False,
-        "default_cache_type": "none",
-        "key": S3_ACCESS_KEY,
-        "secret": S3_SECRET_KEY,
-        "client_kwargs": {'endpoint_url': S3_URL}
-    }
-
-    fs2 = fsspec.filesystem('')  # local file system to save final json to
-    with tempfile.NamedTemporaryFile() as out_json:
-        gen_json(s3_file, fs, fs2, out_json.name, so, storage_type="s3")
+    with fs.open(s3_file, 'rb') as s3file:
+        h5chunks = SingleHdf5ToZarr(s3file, s3_file,
+                                    inline_threshold=0)
 
 
 def test_local_SingleHdf5ToZarr(local_file):
     """Check Kerchunk's SingleHdf5ToZarr when NO S3."""
-    so = dict(mode='rb', anon=True, default_fill_cache=False,
-              default_cache_type='first') # args to fs.open()
-    # default_fill_cache=False avoids caching data in between
-    # file chunks to lower memory usage
     fs = fsspec.filesystem('')
-    fs2 = fsspec.filesystem('')
-    with tempfile.NamedTemporaryFile() as out_json:
-        gen_json(local_file, fs, fs2, out_json.name, so, storage_type="")
-
+    with fs.open(local_file, 'rb') as localfile:
+        h5chunks = SingleHdf5ToZarr(localfile, local_file,
+                                    inline_threshold=0)
