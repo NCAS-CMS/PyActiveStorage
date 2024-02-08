21de682a0d63cbbefac1c51aed5aa2ecbb54fbb0
Valeriu Predoi <valeriu.predoi@gmail.com>
2024-02-08 15:11:10 +0000

    ad hoc mod on config

--- activestorage/config.py
+++ activestorage/config.py
@@ -1,7 +1,7 @@
 # This file contains configuration for PyActiveStorage.
 
 # Whether to use the Reductionist Active Storage server interface.
-USE_S3 = False
+USE_S3 = True
 
 # URL of Reductionist server.
 S3_ACTIVE_STORAGE_URL = "https://192.171.169.248:8080"
@@ -10,13 +10,13 @@ S3_ACTIVE_STORAGE_URL = "https://192.171.169.248:8080"
 S3_ACTIVE_STORAGE_CACERT = None
 
 # URL of S3 object store.
-S3_URL = "http://localhost:9000"
+S3_URL = 'uor-aces-o.s3-ext.jc.rl.ac.uk'
 
 # S3 access key / username.
-S3_ACCESS_KEY = "minioadmin"
+S3_ACCESS_KEY = ""
 
 # S3 secret key / password.
-S3_SECRET_KEY = "minioadmin"
+S3_SECRET_KEY = ""
 
 # S3 bucket.
-S3_BUCKET = "pyactivestorage"
+S3_BUCKET = "bnl"
