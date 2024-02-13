c5ea57aa0d6c5d69da72181ce9904094ef9a3399
Valeriu Predoi <valeriu.predoi@gmail.com>
2024-02-13 15:55:36 +0000

    always print an instance of request data dict from Reductionist

--- activestorage/reductionist.py
+++ activestorage/reductionist.py
@@ -53,6 +53,7 @@ def reduce_chunk(session, server, source, bucket, object,
     """
 
     request_data = build_request_data(source, bucket, object, offset, size, compression, filters, missing, dtype, shape, order, chunk_selection)
+    print("Reductionist request data dictionary:", request_data)
     api_operation = "sum" if operation == "mean" else operation or "select"
     url = f'{server}/v1/{api_operation}/'
     response = request(session, url, request_data)
