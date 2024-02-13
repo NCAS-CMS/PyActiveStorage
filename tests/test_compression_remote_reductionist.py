d51a98a9b03e0a3fe26393a446d6b84ae58f3f4c
Valeriu Predoi <valeriu.predoi@gmail.com>
2024-02-13 15:23:44 +0000

    correct test remote reductionist

--- tests/test_compression_remote_reductionist.py
+++ tests/test_compression_remote_reductionist.py
@@ -12,7 +12,7 @@ from activestorage.dummy_data import make_compressed_ncdata
 import utils
 
 
-# test two ways: local or Bryan's S3 machine + Bryan's reductionist
+# Bryan's S3 machine + Bryan's reductionist
 STORAGE_OPTIONS_Bryan = {
     'anon': True,
     'client_kwargs': {'endpoint_url': "https://uor-aces-o.s3-ext.jc.rl.ac.uk"},
@@ -20,10 +20,9 @@ STORAGE_OPTIONS_Bryan = {
 S3_ACTIVE_URL_Bryan = "https://192.171.169.248:8080"
 # TODO include all supported configuration types
 storage_options_paramlist = [
-    (None, None),
     (STORAGE_OPTIONS_Bryan, S3_ACTIVE_URL_Bryan)
 ]
-
+S3_BUCKET = "bnl"
 
 
 @pytest.mark.parametrize("storage_options, active_storage_url", storage_options_paramlist)
@@ -37,7 +36,14 @@ def test_compression_and_filters_cmip6_data(storage_options, active_storage_url)
         nc_min = np.min(nc_data["tas"][0:2,4:6,7:9])
     print(f"Numpy min from compressed file {nc_min}")
 
-    active = Active(test_file, 'tas', utils.get_storage_type(),
+    # TODO remember that the special case for "anon=True" buckets is that
+    # the actual file uri = bocket + filename (do we want to generalize this in active.py ???)
+    if USE_S3:
+        ofile = os.path.basename(test_file)
+        test_file_uri = os.path.join(S3_BUCKET, ofile)
+    else:
+        test_file_uri = test_file
+    active = Active(test_file_uri, 'tas', utils.get_storage_type(),
                     storage_options=storage_options,
                     active_storage_url=active_storage_url)
     active._version = 1
