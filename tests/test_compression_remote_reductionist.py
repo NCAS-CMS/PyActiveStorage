6030040ce9936aafa42e1477b247ba5fdc74644e
Valeriu Predoi <valeriu.predoi@gmail.com>
2024-02-20 13:27:03 +0000

    fail a test and add v0 test

--- tests/test_compression_remote_reductionist.py
+++ tests/test_compression_remote_reductionist.py
@@ -184,7 +184,7 @@ def test_compression_and_filters_cmip6_forced_s3_using_local_Reductionist():
     assert result == 239.25946044921875
 
 
-def test_compression_and_filters_cmip6_forced_s3_from_local_bigger_file():
+def test_compression_and_filters_cmip6_forced_s3_from_local_bigger_file_v1():
     """
     Test identical to previous
     test_compression_and_filters_cmip6_forced_s3_from_local_2
@@ -219,3 +219,41 @@ def test_compression_and_filters_cmip6_forced_s3_from_local_bigger_file():
     active._method = "min"
 
     result = active[0:2, 0:3, 4:6, 7:9]
+    print(x)
+
+
+def test_compression_and_filters_cmip6_forced_s3_from_local_bigger_file_v0():
+    """
+    Test identical to previous
+    test_compression_and_filters_cmip6_forced_s3_from_local_2
+    but using a bigger file, for more relevant performance measures.
+
+    This is for a special anon=True bucket connected to via valid key.secret
+    Variable standard_name: tendency_of_eastward_wind_due_to_orographic_gravity_wave_drag
+    Variable var_name: m01s06i247_4
+    dims: 30 (time) 39 (plev) 325 (lat) 432 (lon)
+
+    Entire mother file info:
+    [2024-01-24 10:07:02 GMT] 2.8GiB STANDARD da193a_25_day__198807-198807.nc
+    """
+    storage_options = {
+        'key': "f2d55c6dcfc7618b2c34e00b58df3cef",
+        'secret': "$/'#M{0{/4rVhp%n^(XeX$q@y#&(NM3W1->~N.Q6VP.5[@bLpi='nt]AfH)>78pT",
+        'client_kwargs': {'endpoint_url': "https://uor-aces-o.s3-ext.jc.rl.ac.uk"}
+    }
+    active_storage_url = "https://192.171.169.248:8080"
+    bigger_file = "da193a_25_day__198807-198807.nc"
+
+    test_file_uri = os.path.join(
+        S3_BUCKET,
+        bigger_file
+    )
+    print("S3 Test file path:", test_file_uri)
+    active = Active(test_file_uri, 'm01s06i247_4', storage_type="s3",
+                    storage_options=storage_options,
+                    active_storage_url=active_storage_url)
+
+    active._version = 0
+    d = active[0:2, 0:3, 4:6, 7:9]
+    min_result = np.min(d)
+    print(min_result)
