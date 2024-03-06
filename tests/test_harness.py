589c772610f572d86cea2ff9158c2ed5b292fc45
Valeriu Predoi <valeriu.predoi@gmail.com>
2024-03-06 13:59:21 +0000

    fix last test

--- tests/test_harness.py
+++ tests/test_harness.py
@@ -24,7 +24,7 @@ def create_test_dataset(tmp_path):
     return test_file
 
 
-@pytest.mark.xfail(USE_S3, reason="descriptor 'flatten' for 'numpy.ndarray' objects doesn't apply to a 'memoryview' object")
+# @pytest.mark.xfail(USE_S3, reason="descriptor 'flatten' for 'numpy.ndarray' objects doesn't apply to a 'memoryview' object")
 def test_read0(tmp_path):
     """
     Test a normal read slicing the data an interesting way, using version 0 (native interface)
@@ -32,10 +32,10 @@ def test_read0(tmp_path):
     test_file = create_test_dataset(tmp_path)
     active = Active(test_file, 'data', utils.get_storage_type())
     active._version = 0
-    d = active[0:2,4:6,7:9]
-    # TODO Bryan look into this
-    print(d.data, type(d.data))
-    nda = np.ndarray.flatten(d.data)
+    d = active[0:2, 4:6, 7:9]
+    # d.data is a memoryview object in both local POSIX and remote S3 storages
+    # keep the current behaviour of the test to catch possible type changes
+    nda = np.ndarray.flatten(np.asarray(d.data))
     assert np.array_equal(nda,np.array([740.,840.,750.,850.,741.,841.,751.,851.]))
 
 def test_read1(tmp_path):
