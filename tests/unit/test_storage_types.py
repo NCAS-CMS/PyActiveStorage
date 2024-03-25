e99a509f4f774b1135191004d56f993fbf3878bb
Valeriu Predoi <valeriu.predoi@gmail.com>
2024-03-25 13:40:17 +0000

    timeOut for v2, v1 does the pancakes now

--- tests/unit/test_storage_types.py
+++ tests/unit/test_storage_types.py
@@ -180,7 +180,7 @@ def test_reductionist_bad_request(mock_reduce, mock_load, tmp_path):
     make_vanilla_ncdata(test_file)
 
     active = Active(uri, "data", "s3")
-    active._version = 1
+    active._version = 2
     active._method = "max"
 
     with pytest.raises(activestorage.reductionist.ReductionistError):
