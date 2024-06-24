7b0dcfc8f51fd53ba14d5b90d53303b85aff2447
Valeriu Predoi <valeriu.predoi@gmail.com>
2024-06-24 17:07:12 +0100

    add requests as direct dependency

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ REQUIREMENTS = {
         'netcdf4',
         'numcodecs>=0.12',  # github/issues/162
         'numpy!=1.24.3',  # severe masking bug
+        'requests',
         's3fs>=2024.2.0',  # see environment.yml for pin reason
         # pin Zarr to use new FSStore instead of KVStore
         'zarr>=2.13.3', # github.com/zarr-developers/zarr-python/issues/1362
