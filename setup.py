3ce53a59c97c84f497768fa15d35ecd9a8c6443b
Valeriu Predoi <valeriu.predoi@gmail.com>
2024-06-24 15:18:26 +0100

    restore numpy with old pin

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ REQUIREMENTS = {
         'kerchunk>=0.2.4',
         'netcdf4',
         'numcodecs>=0.12',  # github/issues/162
-        # 'numpy!=1.24.3',  # severe masking bug
+        'numpy!=1.24.3',  # severe masking bug
         's3fs>=2024.2.0',  # see environment.yml for pin reason
         # pin Zarr to use new FSStore instead of KVStore
         'zarr>=2.13.3', # github.com/zarr-developers/zarr-python/issues/1362
