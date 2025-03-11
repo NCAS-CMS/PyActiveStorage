33dae930ddc204d478b48273dfa0b0b0ba118189
Valeriu Predoi <valeriu.predoi@gmail.com>
2025-03-11 15:03:44 +0000

    add two auto-storage type tests

--- tests/test_real_https.py
+++ tests/test_real_https.py
@@ -17,6 +17,18 @@ def test_https():
     assert result == np.array([0.6909787], dtype="float32")
 
 
+def test_https_implicit_storage():
+    """Run a true test with a https FILE."""
+    test_file_uri = "https://esgf.ceda.ac.uk/thredds/fileServer/esg_cmip6/CMIP6/AerChemMIP/MOHC/UKESM1-0-LL/ssp370SST-lowNTCF/r1i1p1f2/Amon/cl/gn/latest/cl_Amon_UKESM1-0-LL_ssp370SST-lowNTCF_r1i1p1f2_gn_205001-209912.nc"
+
+    active = Active(test_file_uri, "cl")
+    active._version = 2
+    active._method = "min"
+    result = active[0:3, 4:6, 7:9]
+    print("Result is", result)
+    assert result == np.array([0.6909787], dtype="float32")
+
+
 def test_https_dataset():
     """Run a true test with a https DATASET."""
     test_file_uri = "https://esgf.ceda.ac.uk/thredds/fileServer/esg_cmip6/CMIP6/AerChemMIP/MOHC/UKESM1-0-LL/ssp370SST-lowNTCF/r1i1p1f2/Amon/cl/gn/latest/cl_Amon_UKESM1-0-LL_ssp370SST-lowNTCF_r1i1p1f2_gn_205001-209912.nc"
@@ -24,6 +36,20 @@ def test_https_dataset():
     av = dataset['cl']
 
     active = Active(av, storage_type="https")
+    active._version = 2
+    active._method = "min"
+    result = active[0:3, 4:6, 7:9]
+    print("Result is", result)
+    assert result == np.array([0.6909787], dtype="float32")
+
+
+def test_https_dataset_implicit_storage():
+    """Run a true test with a https DATASET."""
+    test_file_uri = "https://esgf.ceda.ac.uk/thredds/fileServer/esg_cmip6/CMIP6/AerChemMIP/MOHC/UKESM1-0-LL/ssp370SST-lowNTCF/r1i1p1f2/Amon/cl/gn/latest/cl_Amon_UKESM1-0-LL_ssp370SST-lowNTCF_r1i1p1f2_gn_205001-209912.nc"
+    dataset = load_from_https(test_file_uri)
+    av = dataset['cl']
+
+    active = Active(av)
     active._version = 2
     active._method = "min"
     result = active[0:3, 4:6, 7:9]
