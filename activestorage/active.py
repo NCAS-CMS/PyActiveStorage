39e2af541a127dee1a8e562d0b19136ffc7393d2
Valeriu Predoi <valeriu.predoi@gmail.com>
2026-05-27 14:23:49 +0100

    reduce max threads to 10

--- activestorage/active.py
+++ activestorage/active.py
@@ -189,7 +189,7 @@ class Active:
                  ncvar: str = None,
                  axis: tuple = None,
                  interface_type: str = None,
-                 max_threads: int = 100,
+                 max_threads: int = 10,
                  storage_options: dict = None,
                  active_storage_url: str = None,
                  option_disable_chunk_cache: bool = False) -> None:
